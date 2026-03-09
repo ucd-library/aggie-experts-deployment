22563910fac5eafc8dc3a119669d2fe4dcea4c7a
Justin Merz <jrmerz@gmail.com>
2026-03-09 10:42:56 -0700

    helper devops scripts and systemd for prod

---
+++ compose/prod/systemd/README.md
@@ -0,0 +1,14 @@
+aggie-experts.service is a sample script that can be used to register systemd cmds
+
+## Systemd Files
+
+
+```
+sudo systemctl enable /opt/aggie-experts/compose/prod/systemd/aggie-experts.service
+```
+
+Then
+
+```bash
+> sudo service aggie-experts [start|stop] 
+```
---
+++ compose/prod/update-cluster.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+PROJECT_NAME=aggie-experts
+SERVICE_GROUP=$1
+WORKER_COUNT=3
+ALLOWED_SERVICE_GROUPS=("webapp" "anduin" "all")
+WEBAPP_SERVICES=("webapp-gateway" "spa" "api")
+ANDUIN_SERVICES=("dagster-daemon" "dagster-celery-worker" "dagster-ui" "anduin-gateway" "caskfs-ui" "superset")
+
+if [[ -z "$SERVICE_GROUP" ]]; then
+  echo "Usage: $0 <service-group>"
+  echo "Example: $0 webapp"
+  exit 1
+fi
+
+if [[ ! " ${ALLOWED_SERVICE_GROUPS[@]} " =~ " ${SERVICE_GROUP} " ]]; then
+  echo "Error: Invalid service group. Allowed service groups are: ${ALLOWED_SERVICE_GROUPS[@]}"
+  echo "Usage: $0 <service-group>"
+  echo "Example: $0 webapp"
+  exit 1
+fi
+
+set -e
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $ROOT_DIR
+
+git pull
+docker compose -p $PROJECT_NAME pull
+
+# full restart
+if [[ "$SERVICE_GROUP" == "all" ]]; then
+  echo "Performing full restart (down/up) of all services"
+  docker compose -p $PROJECT_NAME down
+  docker compose -p $PROJECT_NAME up --scale dagster-celery-worker=$WORKER_COUNT -d
+
+# attempt to do a rolling restart of just the webapp
+else if [[ "$SERVICE_GROUP" == "webapp" ]]; then
+  echo "Performing rolling restart of webapp services: ${WEBAPP_SERVICES[@]}"
+  docker compose -p $PROJECT_NAME up --no-deps -d ${WEBAPP_SERVICES[@]} 
+
+# for the anduin services
+elif [[ "$SERVICE_GROUP" == "anduin" ]]; then
+  echo "Performing rolling restart of anduin services: ${ANDUIN_SERVICES[@]}"
+  docker compose -p $PROJECT_NAME up --no-deps -d ${ANDUIN_SERVICES[@]} 
+fi
